d634a7cc9665c9bf5dce68f53708720338141dd8
bilguun-erdem0625 <143863121+bilguun-erdem0625@users.noreply.github.com>
2023-10-14 11:30:51 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+//for(var i=0; i<=10;i++){
+	//console.log(i);
+//}
+//for(var j=15; j>=1;j--){
+	//console.log(j);
+//}
+//for(var i=1; i<=10;i++){
+	//if(i%2 == 0){
+		//console.log(i);
+	//}
+//}
+
+//for(var j=1; j<=50;j++){
+	//if(j%3 == 0 && j%5 == 0 ){
+		//console.log(j);
+	//}
+//}
+/*var number=10;
+sum=0;
+for(k=0; k<=number; k++){
+	sum=sum+k;
+}
+console.log(sum);
+
+
+var haha, hehe;
+haha=1;
+hehe =10;
+gg =2/0;
+console.log(gg);*/
+
+/*var a=1;
+
+var too = [1,24245,14524,7786];
+console.log(too);
+console.log(too[2]);
+console.log(too.length);
+too[2]=100;
+
+var test = ["text",5,false,6,true,null];
+console.log(test);
+var myitem = ["gun","sword","axe","shield","helment"];
+myitem.push("map");
+console.log(myitem[0]);
+console.log(myitem[myitem.length-1]);
+for(var i=0; i<myitem[myitem.length-1; i++){*/
+	
+var hehe = [1,2,3,4,5,6,7,8,9,10];
+var j=1;
+	if(j%3 == 0 ){
+console.log(hehe);
+	}
+
+var number=5;
+sum=1;
+for(k=0; k<=number; k++){
+	sum=sum+k;
+}
+console.log(sum);
+
+var omg=5;
+ok=1;
+for(ik=1; ik<=omg; ik++){
+	ok=ok*ik;
+}
+
